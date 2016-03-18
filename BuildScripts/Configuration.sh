476d1781cb73f04e1c60b75d226406f813ea07a3
Ben <ben@partwear.ch>
2016-03-18 06:36:53 +0100

    changed shebang to use bash not sh

--- BuildScripts/Configuration.sh
+++ BuildScripts/Configuration.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export BASE_DIR=/srv/nice.bfh.ch/EdisonPlayGround
 export TMP_DIR=/tmp/oe_temp/
--- BuildScripts/EdisonImageBasic.sh
+++ BuildScripts/EdisonImageBasic.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 source Configuration.sh
 
 cd $BASE_DIR
