29017a53654261c759a4c91c3f53d57de9a36f3f
Ben <ben@partwear.ch>
2016-03-22 21:35:04 +0100

    debug echo

--- BuildScripts/CopyImageToWeb.sh
+++ BuildScripts/CopyImageToWeb.sh
@@ -4,4 +4,5 @@ source BuildScripts/Configuration.sh
 
 cd $BASE_DIR/edison-src/build
 tar -zcvf $ImageName.tar.gz toFlash/*
+echo "Move image to Web: "$ImageName".tar.gz"
 mv $ImageName.tar.gz /var/www/html/
