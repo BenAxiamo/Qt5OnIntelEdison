de5d077afcc8b4b43b3474f7d31db021837ea1e6
BenAxiamo <benjamin.habegger@axiamo.com>
2016-03-22 10:14:44 +0100

    added basedir echo to debug

--- BuildScripts/DownloadOnly.sh
+++ BuildScripts/DownloadOnly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source BuildScripts/Configuration.sh
-
+echo $BASE_DIR
 cd $BASE_DIR
 wget https://downloadmirror.intel.com/24910/eng/edison-src-ww18-15.tgz
 tar -xvf edison-src-ww18-15.tgz
