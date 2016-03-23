5647fa34876c0bb5b95fcd2b6825db3773f0ab15
Ben <ben@partwear.ch>
2016-03-23 14:25:38 +0100

    fixed missing \

--- PatchFiles/PatchBBLayersAddQt5.sh
+++ PatchFiles/PatchBBLayersAddQt5.sh
@@ -4,6 +4,6 @@ source BuildScripts/Configuration.sh
 
 cp PatchFiles/qtbase_5.2.1.bbappend $BASE_DIR/edison-src/meta-qt5/recipes-qt/qt5/
 cd $BASE_DIR/edison-src/build/
-sed -i "/meta-intel-arduino /a $BASE_DIR/edison-src/meta-qt5" $BASE_DIR/edison-src/build/conf/bblayers.conf
+sed -i "/meta-intel-arduino /a 	$BASE_DIR/edison-src/meta-qt5 \\" $BASE_DIR/edison-src/build/conf/bblayers.conf
 
 
