0b7b76aeab7bc14a3a1ab16c826b433db5cf9101
Ben <ben@partwear.ch>
2016-03-23 13:44:48 +0100

    fixed path

--- PatchFiles/PatchBBLayersAddQt5.sh
+++ PatchFiles/PatchBBLayersAddQt5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ../BuildScripts/Configuration.sh
+source BuildScripts/Configuration.sh
 
 cd $BASE_DIR/edison-src/build/
 sed -i "/meta-intel-arduino /a $BASE_DIR/edison-src/meta-qt5" $BASE_DIR/edison-src/build/conf/bblayers.conf
