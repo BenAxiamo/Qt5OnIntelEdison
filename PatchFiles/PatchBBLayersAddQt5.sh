9409df0a9481d48e7b0bd34dbeb7218edfadbc10
Ben <ben@partwear.ch>
2016-03-23 13:41:00 +0100

    added sed script to patch bblayers.confi

---
+++ PatchFiles/PatchBBLayersAddQt5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ../BuildScripts/Configuration.sh
+
+cd $BASE_DIR/edison-src/build/
+sed -i "/meta-intel-arduino /a $BASE_DIR/edison-src/meta-qt5" $BASE_DIR/edison-src/build/conf/bblayers.conf
+
+
