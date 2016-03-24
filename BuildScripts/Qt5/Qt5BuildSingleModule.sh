2255e13d63b49a4fd5ec01156be7e0f9915ab64a
Ben <ben@partwear.ch>
2016-03-24 06:54:58 +0100

    debug output

--- BuildScripts/Qt5/Qt5BuildSingleModule.sh
+++ BuildScripts/Qt5/Qt5BuildSingleModule.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 echo "Base_Dir: "$BASE_DIR
 echo "ImageName: "$ImageName
-
 echo "TMP_DIR:"$TMP_DIR
 
+#we use first argument as recipename, have to change it fo the sourced oe-init script since it expects first argument to be build dir
 recipe = $1
-
-echo "------from shell-------------"
-echo "Arg0: " $0
-echo "Arg1: " $1
-echo "Arg2: " $2
 set -- build
-echo "------before source-------------"
-echo "Arg0: " $0
-echo "Arg1: " $1
-echo "Arg2: " $2
 
 
 source BuildScripts/Configuration.sh
@@ -22,8 +13,4 @@ source BuildScripts/Configuration.sh
 cd $BASE_DIR/edison-src/
 source poky/oe-init-build-env
 bitbake $recipe
-
-
-pwd
-echo "----------------------"
 
