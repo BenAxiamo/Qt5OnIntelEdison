3241209816a8e3602f0be7875aead4a46a6ac272
Ben <ben@partwear.ch>
2016-03-24 06:58:59 +0100

    debug output

--- BuildScripts/Qt5/Qt5BuildSingleModule.sh
+++ BuildScripts/Qt5/Qt5BuildSingleModule.sh
@@ -4,7 +4,7 @@ echo "ImageName: "$ImageName
 echo "TMP_DIR:"$TMP_DIR
 
 #we use first argument as recipename, have to change it fo the sourced oe-init script since it expects first argument to be build dir
-recipe = $1
+recipe=$1
 set -- build
 
 
