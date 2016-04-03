8b028d417582c1963f51bcb9e4348d5d0a7b4b0c
Ben <ben@partwear.ch>
2016-04-03 23:11:06 +0200

    fixed sed expression

--- PatchFiles/PatchBBLayersAddQt5.sh
+++ PatchFiles/PatchBBLayersAddQt5.sh
@@ -4,7 +4,7 @@ source BuildScripts/Configuration.sh
 
 cp PatchFiles/qtbase_5.2.1.bbappend $BASE_DIR/edison-src/meta-qt5/recipes-qt/qt5/
 cd $BASE_DIR/edison-src/build/
-sed -i "/meta-intel-arduino /a 	$BASE_DIR/edison-src/meta-qt5 \\" $BASE_DIR/edison-src/build/conf/bblayers.conf
-sed -i "/meta-qt5 /a 	$BASE_DIR/edison-src/meta-openembedded/meta-ruby \\" $BASE_DIR/edison-src/build/conf/bblayers.conf
+sed -i "/meta-intel-arduino /a 	$BASE_DIR/edison-src/meta-qt5  \\\ " $BASE_DIR/edison-src/build/conf/bblayers.conf
+sed -i "/meta-qt5 /a 	$BASE_DIR/edison-src/meta-openembedded/meta-ruby \\\ " $BASE_DIR/edison-src/build/conf/bblayers.conf
 
 
