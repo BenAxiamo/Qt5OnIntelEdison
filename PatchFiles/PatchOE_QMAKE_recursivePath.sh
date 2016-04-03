b16b529c6be28e5ef8870f518d3e27992f3d1d54
Ben <ben@partwear.ch>
2016-04-03 15:26:31 +0200

    fixed path issues

--- PatchFiles/PatchOE_QMAKE_recursivePath.sh
+++ PatchFiles/PatchOE_QMAKE_recursivePath.sh
@@ -2,8 +2,8 @@
 
 source BuildScripts/Configuration.sh
 
-cp PatchFiles/OE_QMAKE_recursivePath.patch $BASE_DIR/edison-src/build/meta-qt5/
-cd $BASE_DIR/edison-src/build/meta-qt5
+cp PatchFiles/OE_QMAKE_recursivePath.patch $BASE_DIR/edison-src/meta-qt5/
+cd $BASE_DIR/edison-src/meta-qt5
 
 patch -p1 < OE_QMAKE_recursivePath.patch 
 
