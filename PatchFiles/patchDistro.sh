ce64a4205543997c6e847c76bc02020d8fe458cc
Ben <ben@partwear.ch>
2016-04-03 15:28:49 +0200

    fixed filename issues

--- PatchFiles/patchDistro.sh
+++ PatchFiles/patchDistro.sh
@@ -2,9 +2,9 @@
 
 source BuildScripts/Configuration.sh
 
-cp PatchFiles/patchDistro.patch $BASE_DIR/edison-src/meta-intel-edison/meta-intel-edison-distro/conf/distro
+cp PatchFiles/pokyDistroPatch.patch $BASE_DIR/edison-src/meta-intel-edison/meta-intel-edison-distro/conf/distro
 cd $BASE_DIR/edison-src/meta-intel-edison/meta-intel-edison-distro/conf/distro
-patch -c patchDistro.patch
+patch -c pokyDistroPatch.patch
 
 
 
