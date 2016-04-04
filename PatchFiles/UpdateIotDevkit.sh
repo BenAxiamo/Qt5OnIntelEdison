eabb655f97fc90b9c1cf8a5ea2e3d6184ad50122
Ben <ben@partwear.ch>
2016-04-04 22:22:45 +0200

    fixed branch for iot

--- PatchFiles/UpdateIotDevkit.sh
+++ PatchFiles/UpdateIotDevkit.sh
@@ -4,5 +4,5 @@ source BuildScripts/Configuration.sh
 
 cd $BASE_DIR/edison-src/poky/
 rm -rf meta-intel-iot-middleware
-git clone git://git.yoctoproject.org/meta-intel-iot-middleware
+git clone -b daisy git://git.yoctoproject.org/meta-intel-iot-middleware
 
