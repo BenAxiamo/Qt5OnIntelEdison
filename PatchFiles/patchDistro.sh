043c945287d5313c2060c5591bba67f4e036cb48
Ben <ben@partwear.ch>
2016-04-05 10:09:46 +0200

    added logging patch results

--- PatchFiles/patchDistro.sh
+++ PatchFiles/patchDistro.sh
@@ -4,4 +4,6 @@ source BuildScripts/Configuration.sh
 
 cp PatchFiles/pokyDistroPatch.patch $BASE_DIR/edison-src/meta-intel-edison/meta-intel-edison-distro/conf/distro/
 cd $BASE_DIR/edison-src/meta-intel-edison/meta-intel-edison-distro/conf/distro
-patch poky-edison.conf pokyDistroPatch.patch
+patch poky-edison.conf pokyDistroPatch.patch > patch.log
+cat patch.log
+
