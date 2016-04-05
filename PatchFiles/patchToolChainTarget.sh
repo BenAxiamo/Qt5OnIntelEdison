af7518b8e3d12736e0dd1d89682d9c581293f5de
Ben <ben@partwear.ch>
2016-04-05 09:50:41 +0200

    patch apply typo fiy

--- PatchFiles/patchToolChainTarget.sh
+++ PatchFiles/patchToolChainTarget.sh
@@ -4,4 +4,4 @@ source BuildScripts/Configuration.sh
 
 cp PatchFiles/packagegroup-qt5-toolchain-target.patch $BASE_DIR/edison-src/meta-qt5/recipes-qt/packagegroups/
 cd $BASE_DIR/edison-src/meta-qt5/recipes-qt/packagegroups/
-patch nativesdk-packagegroup-qt5-toolchain-host.bb packagegroup-qt5-toolchain-target.patch
+patch packagegroup-qt5-toolchain-target.bb packagegroup-qt5-toolchain-target.patch
